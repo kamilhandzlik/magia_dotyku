00237448cbd6cc05598fc309846d59fc95075524
kamol0209 <kamol0209@gmail.com>
2026-03-31 17:55:01 +0200

    added regulamin-swiadczenia-uslug

---
+++ frontend/app/regulamin-swiadczenia-uslug/page.tsx
@@ -0,0 +1,236 @@
+import Footer from "../components/Footer/Footer";
+
+export default function RegulaminSwiadczenieUslug() {
+  return (
+    <>
+      <main className="max-w-4xl mx-auto px-6 py-12 text-gray-800 leading-relaxed">
+        <h1 className="text-3xl font-bold mb-6">Regulamin świadczenia usług</h1>
+
+        <p className="mb-4">
+          Niniejszy Regulamin określa szczegółowe warunki i zasady świadczenia
+          usług kosmetycznych oraz innych, zwanych dalej Usługami, świadczonych
+          na rzecz Klientów przez Usługodawcę ORI-STYLE Aleksandrę Burczyńskę,
+          ul. Plebiscytowa 48, 44-280 Rydułtowy, NIP 6471587893, REGON
+          368412418, pod adresem ul. Plebiscytowa 48, 44-280 Rydułtowy w Salonie
+          Magii Dotyku Strefa Spa i Relaksu, zwanego dalej Salonem.
+        </p>
+
+        <h2 className="text-xl font-semibold mt-8 mb-4">
+          § 1. Postanowienia ogólne
+        </h2>
+
+        <p className="mb-4">
+          Klient Salonu ma obowiązek zapoznania się z Regulaminem przed
+          skorzystaniem z Usług.
+        </p>
+        <p className="mb-4">
+          Skorzystanie z Usług jest równoznaczne z zapoznaniem się i akceptacją
+          treści niniejszego Regulaminu, bez konieczności jego podpisania.
+        </p>
+
+        <h2 className="text-xl font-semibold mt-8 mb-4">§ 2. Klienci</h2>
+        <p className="mb-4">
+          Klientem Salonu może być pełnoletnia osoba fizyczna.
+        </p>
+        <p className="mb-4">
+          Korzystanie z usług Salonu przez osoby niepełnoletnie, powyżej 13 roku
+          życia, możliwe jest wyłącznie za wyraźną zgodą ich rodzica, opiekuna
+          lub przedstawiciela ustawowego.
+        </p>
+        <h2 className="text-xl font-semibold mt-8 mb-4">
+          § 3. Organizacja pracy
+        </h2>
+        <p className="mb-4">
+          Personel Salonu jest odpowiednio przygotowany do pracy, posiada
+          odpowiednie wykształcenie, wiedzę i doświadczenie w zakresie
+          świadczenia oferowanych Usług.
+        </p>
+        <p className="mb-4">
+          Usługi są wykonywane zgodnie z obowiązującymi procedurami, przy użyciu
+          odpowiednich materiałów, według zaleceń producentów, w odpowiednim
+          czasie, a także po odbyciu przez nich stosownych szkoleń.
+        </p>
+        <p className="mb-4">
+          W Salonie stosowane są materiały jednorazowego użytku lub jeśli są to
+          narzędzia wielokrotnego użytku – odpowiednio zdezynfekowane i
+          wysterylizowane.
+        </p>
+        <p className="mb-4">
+          Na terenie Salonu obowiązuje całkowity zakaz palenia tytoniu,
+          spożywania napojów alkoholowych i środków odurzających.
+        </p>
+        <p className="mb-4">Salon nie świadczy usługi dojazdu do Klientów.</p>
+        <h2 className="text-xl font-semibold mt-8 mb-4">
+          § 4. Świadczenie Usług
+        </h2>
+        <p className="mb-4">
+          Przed przystąpieniem do Usługi personel Salonu przeprowadza z Klientem
+          rozmowę w celu ustalenia, czy stan zdrowia Klienta, przebyte lub
+          obecne choroby nie stanowią przeciwwskazań do wykonania Usługi lub nie
+          stwarzają ryzyka wystąpienia skutków ubocznych.
+        </p>
+        <p className="mb-4">
+          Klient przed skorzystaniem z Usługi ma obowiązek powiadomienia
+          personelu Salonu o wszelkich dolegliwościach i przeciwwskazaniach,
+          które mogą wpłynąć na świadczoną usługę, w szczególności o aktywnych i
+          przebytych chorobach, ciąży lub okresie połogowym, karmieniu piersią,
+          posiadanych protezach, przyjmowanych lekach.
+        </p>
+        <p className="mb-4">
+          Klient zostaje zapoznany z przeciwwskazaniami, skutkami ubocznymi i
+          zaleceniami pozabiegowymi. Skorzystanie z usługi oznacza, że Klient ma
+          ich pełną świadomość i je akceptuje.
+        </p>
+        <p className="mb-4">
+          Salon ma prawo żądania pisemnego potwierdzenia od Klienta o jego
+          stanie zdrowia, a także odmówić wykonania Usługi przy braku woli
+          złożenia takiego oświadczenia przez Klienta albo w przypadku gdy
+          stwierdzi, że istnieją przeciwwskazania do wykonania Usługi
+        </p>
+        <h2 className="text-xl font-semibold mt-8 mb-4">§ 5. Płatności</h2>
+        <p className="mb-4">
+          Klient jest obowiązany do zapłaty za wykonaną Usługę.
+        </p>
+        <p className="mb-4">
+          Cennik usług jest dostępny w Salonie oraz na stronie internetowej
+          https://oristyle.pl/cennik/.
+        </p>
+        <p className="mb-4">
+          Ceny zawarte w cenniku i na stronie internetowej nie są ofertą
+          handlową w rozumieniu przepisów Kodeksu Cywilnego. Salon zastrzega
+          sobie prawo do ich zmiany.
+        </p>
+        <p className="mb-4">
+          W dni wolne od pracy, niedziele i święta istnieje możliwość
+          skorzystania z Usług Salonu – po uprzednim uzgodnieniu z Salonem i za
+          dopłatą równą 100 % wartości Usługi.
+        </p>
+        <p className="mb-4">
+          W Salonie dostępne są następujące sposoby zapłaty: płatność gotówką
+          lub przelewem na telefon BLIK.
+        </p>
+        <p className="mb-4">
+          Salon zastrzega sobie prawo do przeprowadzania czasowych promocji na
+          świadczone Usługi. Skorzystanie z obniżonej ceny lub promocyjnej
+          oferty może być obwarowane odrębnymi zasadami.
+        </p>
+        <h2 className="text-xl font-semibold mt-8 mb-4">
+          § 6. Odpowiedzialność
+        </h2>
+        <p className="mb-4">
+          Personel Salonu nie odpowiada za rzeczy i przedmioty wartościowe
+          pozostawione przez Klienta.
+        </p>
+        <p className="mb-4">
+          Niezastosowanie się Klienta do postanowień Regulaminu lub złożenie
+          nieprawdziwych oświadczeń w przedmiocie swojego stanu zdrowia i
+          przeciwwskazań do wykonania Usługi, skutkuje wyłączeniem
+          odpowiedzialności Salonu za wykonaną Usługę. W szczególności Salon nie
+          ponosi odpowiedzialności za brak efektów zabiegowych, jeśli Klient nie
+          przeszedł nieprzerwanej serii zabiegów, która jest wymagana przez
+          producenta produktów zabiegowych lub gdy Klient został uprzedzony
+          przez personel Salonu o możliwości nieosiągnięcia pożądanych efektów z
+          innych powodów, a mimo to zdecydował się na zabieg. Ponadto Salon nie
+          ponosi odpowiedzialności za uszczerbki na zdrowiu lub/i urodzie po
+          zabiegu wynikające z faktu zatajenia schorzeń i chorób, przyjmowania
+          leków itp. lub powstałych na skutek niestosowania się Klienta do
+          zaleceń pozabiegowych wskazanych przez personel Salonu.
+        </p>
+        <p className="mb-4">
+          Salon nie odpowiada za niezadowolenie Klienta z efektu końcowego
+          usługi, wynikającego z subiektywnych odczuć Klienta, jeśli Usługa
+          została wykonana w sposób profesjonalny, zgodny ze sztuką i ogólnie
+          przyjętymi standardami. Salon nie odpowiada także, że efekty oraz
+          skutki Usługi, o których Klient został poinformowany przed
+          przystąpieniem do Usługi.
+        </p>
+        <h2 className="text-xl font-semibold mt-8 mb-4">§ 7. Rezerwacje</h2>
+        <p className="mb-4">
+          Klient może dokonać rezerwacji Usługi przez system rezerwacji
+          Booksy.com, osobiście lub telefonicznie.
+        </p>
+        <p className="mb-4">
+          Klient jest obowiązany poinformować Salon o ewentualnym spóźnieniu lub
+          nieobecności najpóźniej w dniu poprzedzającym zarezerwowany termin
+          Usługi. W przypadku niestawienia się na umówioną wizytę lub rezygnacji
+          z Usługi w dniu zabiegu – rezerwacja kolejnego terminu będzie możliwa
+          po wcześniejszym dokonaniu przedpłaty.
+        </p>
+        <p className="mb-4">
+          Klient powinien przybyć do Salonu najpóźniej 10 minut przed umówioną
+          godziną.
+        </p>
+        <p className="mb-4">
+          Spóźnienie przekraczające 15 min, bez wcześniejszego poinformowania
+          telefonicznego, nie gwarantuje pełnego wykonania Usługi, a także w
+          pewnych przypadkach może oznaczać odmowę wykonania Usługi.
+        </p>
+        <p className="mb-4">
+          Jeżeli Klient posiadający bon podarunkowy nie pojawi się na umówionej
+          wizycie – nie informując o swojej nieobecności – lub przyjdzie na
+          ustalony termin z chęcią zmiany decyzji o wykonaniu zabiegu (w dniu
+          zabiegu) i przeniesieniu na inny termin – bon traktowany jest jako
+          zrealizowany.
+        </p>
+        <p className="mb-4">
+          Salon zastrzega sobie prawo do odwołania zarezerwowanej Usługi z
+          jednoczesną zmianą terminu rezerwacji lub możliwością umówienia nowego
+          terminu w późniejszym okresie, o czym Klient zostanie wcześniej
+          poinformowany.
+        </p>
+        <h2 className="text-xl font-semibold mt-8 mb-4">§ 8. Reklamacje</h2>
+        <p className="mb-4">
+          Klient ma prawo do złożenia reklamacji sposobu wykonania Usługi w
+          terminie 14 dni od jej wykonania.
+        </p>
+        <p className="mb-4">Klient może zgłosić reklamację w formie pisemne</p>
+        <p>
+          a. osobiście w Salonie Magia Dotyku Strefa Spa i Relaksu pod adresem:
+          ul. Plebiscytowa 48, 44-280 Rydułtowy lub
+        </p>
+        <p>
+          b. pocztą tradycyjną na adres: ORI-STYLE Aleksandra Burczyńska, ul.
+          Plebiscytowa 48, 44-280 Rydułtowy lub
+        </p>
+        <p>
+          c. pocztą elektroniczną na adres e-mail: ola.burczynska@gmail.com.
+        </p>
+        <p className="mb-4">
+          3. Reklamacja powinna zawierać dane identyfikacyjne Klienta (imię,
+          nazwisko, adres korespondencyjny, adres e-mail), opis zgłaszanych
+          zastrzeżeń oraz wskazanie pożądanego przez Klienta sposobu rozwiązania
+          problemu.
+        </p>
+        <p className="mb-4">
+          4. Jeżeli dane lub informacje podane przez Klienta nie są
+          wystarczające do rozpatrzenia reklamacji, Salon niezwłocznie
+          skontaktuje się z Klientem w celu uzupełnienia zaistniałych braków
+          reklamacji.
+        </p>
+        <p className="mb-4">
+          5. Reklamacja jest rozpatrywana niezwłocznie, nie później niż w
+          terminie 14 dni od dnia otrzymania zgłoszenia reklamacji. Salon
+          udziela odpowiedzi na reklamację na piśmie lub drogą elektroniczną, o
+          ile Klient podał adres e-mail, w terminie 14 dni od dnia jej
+          otrzymania. Zaniechanie odpowiedzi w terminie 14 dni traktuje się jako
+          uwzględnienie reklamacji.
+        </p>
+        <p className="mb-4">
+          6. W przypadku nieuwzględnienia reklamacji przez Salon, Klient może
+          dochodzić swoich roszczeń na drodze postępowania sądowego lub
+          skorzystać z pozasądowych sposobów rozpatrywania reklamacji i
+          dochodzenia roszczeń.
+        </p>
+        <h2 className="text-xl font-semibold mt-8 mb-4">
+          § 9. Postanowienia końcowe
+        </h2>
+        <p className="mb-4">
+          W sprawach nieuregulowanych w niniejszym Regulaminie mają zastosowanie
+          powszechnie obowiązujące przepisy prawa polskiego, w szczególności
+          przepisy Kodeksu cywilnego.
+        </p>
+      </main>
+      <Footer />
+    </>
+  );
+}
